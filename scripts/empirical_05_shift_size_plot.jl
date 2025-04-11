a0c9a990dd6a7941409d2747620897f4b487aa2d
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-04-11 11:00:49 +0200

    commit shift size plot

--- scripts/empirical_05_shift_size_plot.jl
+++ scripts/empirical_05_shift_size_plot.jl
@@ -8,7 +8,7 @@ using Pesto
 
 ########################
 ##
-##   some helper functions
+##   read the data files
 ##
 ##########################
 
@@ -42,7 +42,7 @@ d_rates = Dict{String, DataFrame}()
     df = CSV.read(fpath, DataFrame)
     sort!(df, :edge)
     filter!(:edge => x -> x != 0, df) ## remove the root edge
-    is_signif = df[!,:shift_bf] .> 100
+    is_signif = df[!,:shift_bf] .> 10
     df[!,:is_signif] = is_signif
 
     d_rates[name] = df
@@ -83,12 +83,9 @@ end
 
 #heights = [maximum(d.node_depth) for (key, d) in datasets]
 
-
-
-
 ####################################
 ##
-##   Fig. 1: The empirical dataset exploration (not for any of the hypotheses in particular)
+##   Fig. 2: The empirical dataset exploration (not for any of the hypotheses in particular)
 ##
 ####################################
 fig = Figure(size = (480, 280), fontsize = 14, 
@@ -111,16 +108,10 @@ name_subset = [
     "Squamata_Zheng2016",
     "Asteraceae_Palazzesi2022",
     "Agaricomycetes_Varga2019",
-    #"Lissamphibia_Jetz2018",
     "Anura_Portik2023",
-    #"Anthophila_HenriquezPiskulich",
     "Aves_Quintero2022",
     "Polypodiophyta_Nitta2022",
     "Lecanoromycetes_Nelsen2020",
-    #"Primates_Springer2012",
-    #"Sigmodontinae_VallejosGarrido2023",
-    #"Rhopalocera_Kawahara2023",
-    #"Monocots_Howard2019",
 ]
 
 nbins1 = [
@@ -134,9 +125,6 @@ dnames = collect(keys(datasets))
 for dname in dnames
     append!(titles, [split(dname, "_")[1]])
 end
-
-
-#datasets_order = [34, 9, 28, 29, 4]
 
 axs = []
 q = 1
@@ -177,17 +165,12 @@ for i in 1:3, j in 1:4
     end
 end
 
-
-
-
 for (q, name) in enumerate(name_subset)
     model = models[name]
     println(q, "\t", name)
 
-    #Nmatrix = N[dataset_index,:,:,1]
     N_signif = d[name]["N"][d_rates[name][!,:is_signif],:,:]
     Nmatrix = sum(N_signif, dims = 1)[1,:,:]
-    #nbins = 20
     nbins = nbins1[q]
 
     Ns = zeros(3,nbins)
@@ -244,8 +227,6 @@ leg = Legend(g[1,4], elements, labels, labelsize=9,
 leg.tellheight = true
 leg.tellwidth = true
 
-
-#linkxaxes!(axs...)
 linkxaxes!(axs[1], axs[4], axs[8])
 linkxaxes!(axs[2], axs[5], axs[9])
 linkxaxes!(axs[3], axs[6], axs[10])
@@ -270,19 +251,15 @@ rowsize!(g, 4, Relative(0.1))
 colgap!(g, 5)
 rowgap!(g, 3)
 
-
-#linkaxes!(ax_scatter1, ax_scatter2)
-
 fig
 #set_theme!(fig, figure_padding = 0)
-#CairoMakie.save("figures/fig1_empirical.pdf", fig)
-#CairoMakie.save("figures/histogram_magnitude_etaoneshift.pdf", fig)
+#CairoMakie.save("figures/fig1_empirical_significant.pdf", fig)
 
-
-
-###
+################################
+##
 ## plot each phylogeny individually
 ##
+################################
 
 
 for name in keys(models)  
@@ -361,6 +338,8 @@ end
 
 fig3 = Figure(size = (800, 800));
 
+colors = [:steelblue, "orange", "gray"]
+
 global q = 1
 qnames = [keys(models)...]
 
@@ -374,8 +353,8 @@ end
 
 
 axs = []
-for i in 1:7
-    for j in 1:7
+for i in 1:5
+    for j in 1:5
         if q <= length(keys(models))
             ax = Axis(fig3[i,j], 
                 xgridvisible = false, 
@@ -395,6 +374,12 @@ end
 
 i = 1
 for name in keys(models)
+    rates = deepcopy(d_rates[name])
+    filter!(row -> row[:shift_bf] > 10, rates)
+
+    hist!(axs[i], rates[!,:delta_netdiv])
+   
+    #=
     model = models[name]
     Nmatrix = sum(d[name]["N"], dims = 1)[1,:,:]
     nbins = 20
@@ -446,19 +431,325 @@ for name in keys(models)
         color = [colors[x] for x in bar_df[!, "subset"]],
         )
        
+        =#
     i += 1
 end
 
 ## add x-axis label and y-axis label
-ylabel = Label(fig3[1:7, 0], L"\text{number of rate shifts }(\hat{N})", rotation = π/2)
-xlabel = Label(fig3[8, 1:7], L"\text{shift size in net diversification }(\Delta r)")
+ylabel = Label(fig3[1:5, 0], L"\text{number of rate shifts }(\hat{N})", rotation = π/2)
+xlabel = Label(fig3[6, 1:5], L"\text{shift size in net diversification }(\Delta r)")
 
 colgap!(fig3.layout, 4)
 rowgap!(fig3.layout, 2)
 
+fig3
+
 CairoMakie.save("figures/shiftsize_free_xlimit.pdf", fig3)
 #CairoMakie.save("figures/shiftsize_fixed_xlimit.pdf", fig3)
 
+########################
+##
+## Plot the number of significant rate shift events
+## as a function of the groups
+##
+########################
+
+
+kingdom = Dict{String, String}()
+
+kingdom["Actinopterygii_Rabosky2018"] = "Animalia"
+kingdom["Agaricomycetes_Varga2019"] = "Fungi"
+kingdom["Anthophila_HenriquezPiskulich"] = "Animalia"
+kingdom["Anura_Portik2023"] = "Animalia"
+kingdom["Aristolochiaceae_Allio2021"] = "Plantae"
+kingdom["Asteraceae_Palazzesi2022"] = "Plantae"
+kingdom["Aves_Quintero2022"] = "Animalia"
+kingdom["Caryophyllales_Smithetal2017_align_w_shtest_treepl_3samp"] = "Plantae"
+kingdom["Chondrichthyes_Stein2018"] = "Animalia"
+kingdom["Cornales_Rose2018"] = "Plantae"
+kingdom["Cycadaceae_Liu2021"] = "Plantae"
+kingdom["Ericales_Rose2018"] = "Plantae"
+kingdom["Gesnerioideae_SerranoSerrano2017"] = "Plantae"
+kingdom["Lecanoromycetes_Nelsen2020"] = "Fungi"
+kingdom["Mammalia_AlvarezCarretero2022"] = "Animalia"
+kingdom["Orobanchaceae_Mortimer2022"] = "Plantae"
+kingdom["Phasmatodea_Bank2022"] = "Animalia"
+kingdom["Pinophyta_Leslie2018"] = "Plantae"
+kingdom["Poaceae_Spriggs2015"] = "Plantae"
+kingdom["Polypodiophyta_Nitta2022"] = "Plantae"
+kingdom["Rhopalocera_Kawahara2023"] = "Animalia"
+kingdom["Rosidae_Sun2020"] = "Plantae"
+kingdom["Salvia_Kriebel2019"] = "Plantae"
+kingdom["Squamata_Zheng2016"] = "Animalia"
+
+
+
+
+rainclouds_datasets = reverse([
+    "Actinopterygii_Rabosky2018",
+    "Agaricomycetes_Varga2019",
+    "Anthophila_HenriquezPiskulich",
+    "Anura_Portik2023",
+    "Aristolochiaceae_Allio2021",
+    "Asteraceae_Palazzesi2022",
+    "Aves_Quintero2022",
+    "Caryophyllales_Smithetal2017_align_w_shtest_treepl_3samp",
+    "Chondrichthyes_Stein2018",
+    "Cornales_Rose2018",
+    "Cycadaceae_Liu2021",
+    "Ericales_Rose2018",
+    "Gesnerioideae_SerranoSerrano2017",
+    "Lecanoromycetes_Nelsen2020",
+    "Mammalia_AlvarezCarretero2022",
+    "Orobanchaceae_Mortimer2022",
+    "Phasmatodea_Bank2022",
+    "Pinophyta_Leslie2018",
+    "Poaceae_Spriggs2015",
+    "Polypodiophyta_Nitta2022",
+    "Rhopalocera_Kawahara2023",
+    "Rosidae_Sun2020",
+    "Salvia_Kriebel2019",
+    "Squamata_Zheng2016",
+])
+
+colormap = Dict("Animalia" => :gray, "Plantae" => (:green, 0.8), "Fungi" => (:orange, 0.8))
+color = [colormap[kingdom[name]] for name in rainclouds_datasets]
+
+rainclouds_labels = reverse([
+    L"\text{Actinopterygii}",
+    L"\text{Agaricomycetes}",
+    L"\text{Anthophila}",
+    L"\text{Anura}",
+    L"\text{Aristolochiaceae}",
+    L"\text{Asteraceae}",
+    L"\text{Aves}",
+    L"\text{Caryophyllales}",
+    L"\text{Chondrichthyes}",
+    L"\text{Cornales}",
+    L"\text{Cycadaceae}",
+    L"\text{Ericales}",
+    L"\text{Gesnerioideae}",
+    L"\text{Lecanoromycetes}",
+    L"\text{Mammalia}",
+    L"\text{Orobanchaceae}",
+    L"\text{Phasmatodea}",
+    L"\text{Pinophyta}",
+    L"\text{Poaceae}",
+    L"\text{Polypodiophyta}",
+    L"\text{Rhopalocera}",
+    L"\text{Rosidae}",
+    L"\text{Salvia}",
+    L"\text{Squamata}",
+])
+
+fig2 = Figure(size = (500, 350))
+
+yt = collect(1:length(rainclouds_datasets)/2)
+ytl1 = rainclouds_labels[1:12]
+ytl2 = rainclouds_labels[13:24]
+
+ax1 = Axis(fig2[1,1], 
+    #xlabel = L"\text{shift size in net diversification }(\Delta r)", 
+    xgridvisible = false, ygridvisible = false,
+    yticks = (yt, ytl2),
+    topspinevisible = false,
+    rightspinevisible = false,
+    xticklabelrotation = π/2,
+    xticklabelsize = 9,
+    yticklabelsize = 9)
+
+ax2 = Axis(fig2[1,2], 
+    #xlabel = L"\text{shift size in net diversification }(\Delta r)", 
+    xgridvisible = false, ygridvisible = false,
+    yticks = (yt, ytl1),
+    topspinevisible = false,
+    rightspinevisible = false,
+    xticklabelrotation = π/2,
+    xticklabelsize = 9,
+    yticklabelsize = 9)
+
+    
+Label(fig2[2, 1:2], L"\text{shift size in net diversification }(\Delta r)", rotation = 0)
+
+
+shifts = Dict{String, Vector{Float64}}()
+for (name, rates) in d_rates
+    rates_signif = rates[rates[!,:is_signif],:]
+    shifts[name] = rates_signif[!,:delta_netdiv]
+end
+
+xs1 = Float64[]
+ys1 = Float64[]
+
+xs2 = Float64[]
+ys2 = Float64[]
+wc = Makie.wong_colors()
+#colors = reverse([:gray, :gray, :gray, :gray, :gray, :gray, wc[3], wc[3], wc[3], wc[3], wc[4], wc[4]])
+cs1 = []
+cs2 = []
+for (i, name) in enumerate(rainclouds_datasets[1:12])
+    for item in shifts[name]
+        push!(xs1, i)
+        push!(ys1, item)
+        push!(cs1, color[i])
+    end
+end
+
+for (i, name) in enumerate(rainclouds_datasets[13:24])
+    for item in shifts[name]
+        push!(xs2, i)
+        push!(ys2, item)
+        push!(cs2, color[13:24][i])
+        #push!(cs, colors[i])
+    end
+end
+
+
+rainclouds!(ax2, xs1 .+ 0.25, ys1, 
+    clouds = nothing, 
+    #color = :gray,
+    color = cs1,
+    boxplot_width = 0.5, cloud_width = 0.5,
+    orientation = :horizontal,
+    jitter_width=0.35,
+    side_nudge = 0.55,
+    markersize=3,
+    )
+
+rainclouds!(ax1, xs2 .+ 0.25, ys2, 
+    clouds = nothing, 
+    #color = :gray,
+    color = cs2,
+    boxplot_width = 0.5, cloud_width = 0.5,
+    orientation = :horizontal,
+    jitter_width=0.35,
+    side_nudge = 0.55,
+    markersize=3,
+    )
+
+linkxaxes!(ax1, ax2)
+
+
+Legend(fig2[1,3], 
+    [
+        MarkerElement(color = :gray, marker = :circle, markersize = 9),
+        MarkerElement(color = :green, marker = :circle, markersize = 9),
+        MarkerElement(color = :orange, marker = :circle, markersize = 9),
+    ],
+    ["Animalia", "Plantae", "Fungi"],
+    patchsize = (8, 8), 
+    rowgap = 5,
+)
+
+for i in 1:2
+    colsize!(fig2.layout, i, Relative(0.4))
+end
+colsize!(fig2.layout, 3, Relative(0.2))
+
+fig2
+
+
+save("figures/all_datasets_shiftsizes.pdf", fig2)
+
+########################
+##
+## Plot the number of significant rate shift events
+## as a function of the clades
+##
+########################
+
+
+name_subset2 = [
+    "Actinopterygii_Rabosky2018",
+    "Mammalia_AlvarezCarretero2022",
+    "Rosidae_Sun2020",
+    "Polypodiophyta_Nitta2022",
+    "Chondrichthyes_Stein2018",
+    "Squamata_Zheng2016",
+    "Asteraceae_Palazzesi2022",
+    "Agaricomycetes_Varga2019",
+    "Anura_Portik2023",
+    "Aves_Quintero2022",
+    "Poaceae_Spriggs2015",
+    "Lecanoromycetes_Nelsen2020",
+]
+
+
+
+fig3 = Figure(size = (480, 280), fontsize = 14, 
+            figure_padding = (1,1,1,1));
+
+nbins1 = [
+    20, 20, 10, 10,
+    4, 6, 20, 18,
+    6, 14, 4, 20
+]
+
+axs = []
+q = 1
+for i in 1:3, j in 1:4
+    if i < 3
+        xlabel = ""
+    else
+        xlabel = L"\Delta r"
+    end
+
+    title = split(name_subset2[q], "_")[1]
+
+    ax = Axis(fig3[i,j], 
+    xgridvisible = false, 
+    ygridvisible = false,
+    title = title,
+    titlesize = 9,
+    topspinevisible = false,
+    rightspinevisible = false,
+    xticklabelrotation = π/2,
+    xticklabelsize = 9,
+    yticklabelsize = 9)
+
+    if i < 3
+        hidexdecorations!(ax, ticks = false)
+    end
+
+    append!(axs, [ax])
+    q += 1
+end
+
+nbins3 = [
+    20, 20, 14, 20,
+    2, 6, 20, 4,
+    7, 14, 10, 10
+]
+
+for (i, name) in enumerate(name_subset2)
+    ax = axs[i]
+    hist!(ax, shifts[name], color = :gray, bins = nbins3[i])
+end
+
+linkxaxes!(axs[1], axs[5], axs[9])
+linkxaxes!(axs[2], axs[6], axs[10])
+linkxaxes!(axs[3], axs[7], axs[11])
+linkxaxes!(axs[4], axs[8], axs[12])
+
+ylabel = Label(fig3[1:3, 0], L"\text{no. significant rate shift events}", rotation = π/2)
+xlabel = Label(fig3[4, 1:4], L"\text{shift size in net diversification }(\Delta r)")
+
+for col in 1:3
+    colsize!(fig3.layout, col, Relative(0.23))
+end
+colsize!(fig3.layout, 0, Relative(0.05))
+
+for i in 1:3
+    rowsize!(fig3.layout, i, Relative(0.30))
+end
+rowsize!(fig3.layout, 4, Relative(0.1))
+
+colgap!(fig3.layout, 5)
+rowgap!(fig3.layout, 3)
+
+fig3
+
+save("figures/fig1_empirical_atomized.pdf", fig3)
 
 
 
